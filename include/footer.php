e903b97c62d26cfc0a3cdc815af915138743d2ee
Rakul-netizen <rakul7168@gmail.com>
2025-06-20 17:32:30 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,71 @@
+<footer class="footer" aria-label="Secondary-navigation">
+  <div class="container">
+    <div class="rowz">
+      <!-- Column 1 -->
+      <div class="footer_col">
+        <img src="./asset/logo/logo.png" alt="Jobaskit Logo" class="footer_logo mb-2" />
+        <p class="small">
+            <strong>India’s 1st Pay-As-You-Use Recruitment Portal</strong>
+            <br>Unlock tailored placement opportunities from leading employers. Showcase your skills, connect directly with recruiters, and take the next step in your career through a platform dedicated to your success.
+            </p>
+        <ul class="social_icons">
+          <li><a href="https://www.facebook.com/Jobaskit"><i class="fa-brands fa-facebook-f text-light"></i></a></li>
+          <li><a href="https://twitter.com/Jobaskit"><i class="fa-brands fa-x-twitter text-light"></i></a></li>
+          <li><a href="https://www.instagram.com/jobaskit/"><i class="fa-brands fa-instagram text-light"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-linkedin-in text-light"></i></a></li>
+          <li><a href="https://youtube.com/@Jobaskit"><i class="fa-brands fa-youtube text-light"></i></a></li>
+          <li><a href="https://www.threads.net/@jobaskit"><i class="fa-brands fa-threads text-light"></i></a></li>
+        </ul>
+      </div>
+
+      <!-- Remaining Columns -->
+      <div class="footer_col">
+        <h4>Know Us</h4>
+        <ul class="list-unstyled small">
+          <li><a href="https://www.makosinfotech.com/about_us.php">About</a></li>
+          <li><a href="https://www.makosinfotech.com/management_team.php">Team</a></li>
+          <li><a href="#">Career</a></li>
+          <li><a href="https://www.makosinfotech.com/invest_with_us.php">Invest with us</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <div class="footer_col">
+        <h4>Resources</h4>
+        <ul class="list-unstyled small">
+          <li><a href="https://training.jobaskit.com/">Training</a></li>
+          <li><a href="https://recruiter.jobaskit.com/">Recruiter</a></li>
+          <li><a href="https://jobseeker.jobaskit.com/">Jobseeker</a></li>
+          <li><a href="https://www.jobaskit.com/blog/index.php">Blog</a></li>
+          <li><a href="#">Help Center</a></li>
+        </ul>
+      </div>
+
+      <div class="footer_col">
+        <h4>Policies</h4>
+        <ul class="list-unstyled small">
+          <li><a href="tca.php">TCA</a></li>
+          <li><a href="privacypolicy.php">Privacy Policy</a></li>
+          <li><a href="refundpolicy.php">Refund Policy</a></li>
+        </ul>
+      </div>
+
+      <div class="footer_col">
+        <h4>Partner with Us</h4>
+        <ul class="list-unstyled small">
+          <li><a href="training/">Training partner</a></li>
+          <li><a href="#">Business partner</a></li>
+          <li><a href="#">Sales partner</a></li>
+          <li><a href="#">Influencers/Affiliates</a></li>
+          <li><a href="student_partner.php">Student partner</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="text-center pt-3 border-top border-secondary mt-3">
+      <small class="d-block">
+        © 2025 Makos Infotech Pvt. Ltd. – All rights reserved. Powered by <strong>Makos Infotech Private Limited</strong> | Since 2020
+      </small>
+    </div>
+  </div>
+</footer>
--- include/navbar.php
+++ include/navbar.php
@@ -1,1 +1,35 @@
-
+<nav class="nav" role="navigation" aria-label="Main menu">
+    <div class="nav__container container">
+        <div class="nav__logo">
+            <img src="./asset/logo/logo.png" loading="lazy" alt="Company logo">
+        </div>
+        <button id="menu-bar" class="nav__menu-button" aria-label="Toggle-menu" aria-expanded="false"
+            aria-controls="nav-links">
+            <i class="bi bi-list"></i>
+        </button>
+        <div class="nav__modules">
+            <ul class="nav__links" id="nav-links" role="menubar">
+                <li class="nav__item" role="menuitem" tabindex="0">
+                    <a href="" class="nav__link">Home</a>
+                </li>
+                <li class="nav__item" role="menuitem" tabindex="0">
+                    <a href="" class="nav__link">Training</a>
+                </li>
+                <li class="nav__item" role="menuitem" tabindex="0">
+                    <a href="../recruiter/login.php" class="nav__link">Recruiter</a>
+                </li>
+                <li class="nav__item" role="menuitem" tabindex="0">
+                    <a href="" class="nav__link">Institutes</a>
+                </li>
+                <ul class="nav__links-buttons">
+                    <li class="nav__item" role="menuitem" tabindex="0">
+                        <a href="" class="nav__link nav__link-button">Login</a>
+                    </li>
+                    <li class="nav__item" role="menuitem" tabindex="0">
+                        <a href="" class="nav__link nav__link-button">Sign Up</a>
+                    </li>
+                </ul>
+            </ul>
+        </div>
+    </div>
+</nav>
